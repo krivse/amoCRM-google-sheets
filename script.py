1bff720e262c91aa3f2c7eb6115e7ce67485a3f4
krivse <valones75@gmail.com>
2022-12-26 18:12:16 +0300

    cleaning

--- script.py
+++ script.py
@@ -15,19 +15,18 @@ load_dotenv(dotenv_path)
 class ManagerAmoCRM:
     """
     Класс реализует подключение к amoCRM с помощью протокола Oauth2.
+    Нужно на вход передать необходимые ключи для подключения.
     Токены и другие переменные автоматически обновляются и записаны в файл .env.
-    !!! Файл myутм.env необходимо переименовать в .env
+    !!! Файл myenv.env необходимо переименовать в .env
     Метод events_request() реализует сбор данных: events,
     аргументы на вход: filters, id_client, current_day
-    По умолчанию выставлены аргументы
-
+    По умолчанию установлены значения.
     В первый запрос необходимо передать текущий день для аргумента current_day
     C его помощью будет происходить запись в ячейку google_shits каждый новый день
     При создании новой формы или обновлении текущей - нужно снова передать current_day
     Значение сохраняется в файл .env из которого происходит перерасчёт дней.
-
     Передача аргументов для выборки:
-        None or '' = получение всех ['events']
+        '' = получение всех ['events']
         param_id=str(id) - необязательный аргумент, который по умолчанию ''
         filters=str(filters, page, limit) - необязательный аргумент, который по умолчанию ''
     """
@@ -42,11 +41,15 @@ class ManagerAmoCRM:
         self._lifetime_token = None
         self._time_now = time.time()
         self._expires_in = None
-        self.filters = ''
-        self.param_id = '/'
         self._connected()
 
     def _connected(self):
+        """
+        Первичное подключение к amoCRM и получение токена и записывается .env,
+        откуда в дальнейшем будет вызываться.
+        Если подключение было произведено ранее, то выполняется get_запрос
+        Если access токен истёк - получаем новый и перезаписываем в .env
+        """
         if self._code != os.getenv('CODE') or os.getenv('CODE') == '':
             data = {
                 "grant_type": "authorization_code",
@@ -64,12 +67,13 @@ class ManagerAmoCRM:
                     raise Exception(response.json()['hint'])
                 response = response.json()
                 self._get_tokens(response)
-        if self._time_now - float(os.getenv('LIFETIME_TOKEN')) <= int(os.getenv('EXPIRES_IN')):
+        elif self._time_now - float(os.getenv('LIFETIME_TOKEN')) >= int(os.getenv('EXPIRES_IN')):
             self._update_tokens()
         else:
             return
 
     def _get_tokens(self, response):
+        """Записываем необходимые данные о токене в .env"""
         self._lifetime_token = dotenv.set_key(dotenv_path, 'LIFETIME_TOKEN', str(time.time()))
         self._code = dotenv.set_key(dotenv_path, 'CODE', self._code)
         self._access_token = dotenv.set_key(dotenv_path, 'ACCESS_TOKEN', response['access_token'])
@@ -78,6 +82,7 @@ class ManagerAmoCRM:
         return
 
     def _update_tokens(self):
+        """Обновление токенов с последующей их записью в .env."""
         body = {
             "client_id": self._client_id,
             "client_secret": self._client_secret,
@@ -94,13 +99,19 @@ class ManagerAmoCRM:
         raise EnvironmentError(f'Cant refresh token {response.json()}')
 
     def events_request(self, filters='', param_id='', current_day=None):
+        """
+        :param '' : get запрос на выборку всех данных ['events']
+        :param filters: str(filters, page, limit)
+        :param param_id: str(id)
+        :param current_day: Рассчёт прошедших дней
+        """
         if self._access_token is None:
             token = os.getenv("ACCESS_TOKEN")
         else:
             token = self._access_token[2]
         params = {'Authorization': f'Bearer {token}'}
         if param_id != '':
-            response = requests.get(f'https://ivanskywalker.amocrm.ru/api/v4/events/{param_id}', headers=params)  # ?filter[created_at][from]={1671898064}&filter[created_at][to]={1671912877}
+            response = requests.get(f'https://ivanskywalker.amocrm.ru/api/v4/events/{param_id}', headers=params)
             events = response.json()
             created_at = events['created_at']
             value = self._next_day(created_at, current_day)
@@ -114,21 +125,20 @@ class ManagerAmoCRM:
 
     @staticmethod
     def _next_day(event_day, current_day):
-        if os.getenv("RECORDING_PERIOD") == '' or not current_day is None:
+        """Функция рассчёта дней для google-sheets."""
+        if os.getenv("RECORDING_PERIOD") == '' or current_day is not None:
             dotenv.set_key(dotenv_path, 'RECORDING_PERIOD', str(current_day))
             return current_day - date.fromtimestamp(event_day).day
         else:
-            return int(os.getenv("RECORDING_PERIOD")) - date.fromtimestamp(event_day).day + 2
+            return int(os.getenv("RECORDING_PERIOD")) - date.fromtimestamp(event_day).day + 1
 
 
 if __name__ == "__main__":
     app = ManagerAmoCRM(
-        '4ded840c-b376-4608-980c-3bdfe48da4a3',
-        'VmY27yuuXfTwufoqFSZFNkS1hHfm7TpsT1E5THa7ZtJRvZMSVbj9AatKxeI5KrqT',
+        'a7bdd0ee-9e88-4c02-aef5-3858c2c1a72a',
+        'MO10joBN5Ennvn1bykSo3joFPxgX6D1wJOkkNPvrMHj6nVOzprdYOkwmiyBPsFeg',
         'ivanskywalker',
-        'def50200d9544c4949d68bbd7ee9c7471ed77f01b9b10639673de1f82e58b1a017db3e24316912fe0ea91c0cffd501ac54012b462b593d46d862485ec28b38337adbb4b829cd03b08704177dea7659485a0b5f0033b9e9a2b55030b5114233ea9fb799a53041fc3187803bad5f5cac5defea3141bbcfcaac1b05d2e50d9cd39e120de8529bcd060d30deebe096e439d24f4108ba1c8d0b2bd46f065092769b60f2496c455243e56b3f4566456cccf584f9a3596d9d5e39c2e14d8b1da9dab25b81664b94ec9a9ca60d0fdd26d4fa43c232a285446c66ca1e051227a0e293a7830806f131f4b2e9b5c30201a94d7d18d21382799f3a5aaafad1d2fe0f9ff730cb122c4f45ac7df53ecdcabd9630b4e7a802922682a81937ce8aaa72fdc12b8e90dd7ad88cbf478aeab0ce423d47edc98b3f8131da540724d31e12c4cb46f9d437ffb5317f99796b82d50ccaf7cb30298020392776fb40c9e864b1fcd8915e8474f6425cb3f07a84e79e9601651e1feb48fe4098a88e54328116527a9f5927ded9056bd0177d21a3d68ef0338cb6a7946d2974ea89cbbd664a2e8ea6e5cc501d350eac0a274b3fa9f5938a273fe4f692a56beeffcbb3d3c2fa71af6c4059145158dd8fe5b996c3daede370a94af111bdd6a1',
+        'def50200173e02773a65adf6e8df2bbec5e0a0b98f17e08692b11d6a2c3787de6a202d7a09a811bdfbd667666dd7e991d3786958ce8ec914a655ca7e811cc4d10fadebaf3b7db6fa1c558e803e13c46427c4d69689ee184a7fe6ef45b5d795bd29200d3c5b19719baa4a39955ec17b22b07867248cab82719d4ece82e72ddf9b0cf7bf49fb086f44109ed1e5104790ac9aa944168d080177e2fcd32874ab2ba251ad0a29a749b1db0fe3247ca9c30cd91aa2537f5b5be3813a979696800c8a982dbb454475b748ee8632a961b373a22e2dd7d99a8d0934d7801b80c27094824c1b94c8840a80e2ecd14e10598263228d944e20b496c8e363655882956a2919a9dbd77974b0620d878278c5e99ba790a9e2648a5e62371f458f7eda9beaf76c44f57709cee6837e8dfe2c8ac8438f90fe1d1ddee21f7a8e9152c306aab09293e40d1dbb7550cf30d17262ba9b2b98009243e270094c3f91aefa55517d28a248d67432e70c5396c72c7be529fed4d8f1737be441c8f18eea36786c4dd569c569cee09084bcbf66dcb4b0724f18fecb2f161c3cd120c5d020873bfa0c42fc84aee29c51fd8cc781e9656a39001040005f11578a882f6b9479ceaa9cc6abb9df840396fb908398c494292e2f2a3f628c90ff34',
         'https://test.com'
         )
-    app.events_request()  # None or ''=get['events'], param_id=str(id), filters=str(filters, page, limit), day=день с какого начинается отчёт, передаётся один, если запроос меняется, то пердаётся заного
-
-
+    app.events_request()
